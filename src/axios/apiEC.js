21dbd02c6991a33d491738cb929b09c07b46f153
GeniusPudding <r08922107@ntu.edu.tw>
2021-03-03 12:37:52 +0800

    change API domain to https://ai.franklin.com.tw/

--- src/axios/apiEC.js
+++ src/axios/apiEC.js
@@ -3,7 +3,7 @@ import axios from "axios";
 
 // const ECLoginbaseURL = location.hostname == 'etrade.fraklin.com.tw' ? 'https://etrade.fraklin.com.tw/v2' : 'https://wt.franklin.com.tw:8080/v2'
 const ECbaseURL = location.hostname == 'localhost' 
-    ? 'http://10.20.1.7/ec/' : 'http://210.65.139.185/ec/'
+    ? 'http://10.20.1.7/ec/' : 'https://ai.franklin.com.tw/ec/' // 'http://210.65.139.185/ec/'
 export default function $axios(options) {
     console.log('EC options:', options)
     return new Promise((resolve, reject) => {
